c1cd9a0bbe72969ef7c39680b349dccf2d73d21a
mingda.cai <mingda.cai@valid.com>
2020-03-14 17:31:27 +0800

    Linked list in java

---
+++ Projecteuler/LinkedList/Java/LinkedList/src/dataStructs/LinkedList.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package dataStructs;
+
+/**
+ * @author mingda.cai
+ *
+ */
+public class LinkedList<T> {
+
+	public Node head;
+	public Node tail;
+	
+	public class Node {
+		T data;
+		Node next;
+		
+		Node(T value) {
+			data = value;
+			next = null;
+		}
+	}
+	
+	public void append(T data) {
+		Node newNode = new Node(data);
+		newNode.next = null;
+		
+		if(head == null) {
+			head = newNode;
+			tail = head;
+		}
+		else {
+			while(tail.next != null) {
+				tail = tail.next;
+			}
+			tail.next = newNode;			
+			tail = newNode;
+		}
+	}
+	
+	public boolean insert(int index, T data) {
+		Node newNode = new Node(data);
+		if(index == 0) {
+			newNode.next = head;
+			head = newNode;
+		}
+		else {
+			Node iteratorNode = head;
+			Node tempNode;
+			while(index != 1) {
+				if(iteratorNode.next != null) {
+					iteratorNode = iteratorNode.next;
+					index --;
+				}
+				else {
+					return false;
+				}
+			}
+			tempNode = iteratorNode.next;
+			iteratorNode.next = newNode;
+			newNode.next = tempNode;
+		}
+		return true;
+	}
+	
+	public boolean delete(int index) {
+		Node iteratorNode = head;
+		if(index == 0) {
+			head = head.next;
+		}
+		else {
+			while(index != 1) {
+				if(iteratorNode.next != null) {
+					iteratorNode = iteratorNode.next;
+					index --;
+				}
+				else {
+					return false;
+				}
+			}
+			iteratorNode.next = iteratorNode.next.next;
+		}
+		return true;
+	}
+	
+	public int size() {
+		int i=1;
+		Node iteratorNode = head;
+		while(iteratorNode != tail) {
+			iteratorNode = iteratorNode.next;
+			i++;
+		}
+		return i;
+	}
+	
+}
---
+++ Projecteuler/LinkedList/Java/LinkedList/src/dataStructs/Test.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package dataStructs;
+
+/**
+ * @author mingda.cai
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> intLinkedList = new LinkedList<Integer>();
+		
+		intLinkedList.append(1);
+		intLinkedList.append(2);
+		intLinkedList.append(3);
+		intLinkedList.append(4);
+		intLinkedList.append(5);
+		intLinkedList.append(6);
+		intLinkedList.append(7);
+		intLinkedList.append(8);
+		intLinkedList.append(9);
+		intLinkedList.append(10);
+		intLinkedList.insert(0, 0);
+		intLinkedList.insert(1, 1);
+		intLinkedList.insert(1, 1);
+		intLinkedList.delete(1);
+		intLinkedList.delete(1);
+		
+		LinkedList<Integer>.Node currNode = intLinkedList.head;
+		
+		while(currNode != null) {
+			System.out.print(currNode.data + " ");
+			currNode = currNode.next;
+		}
+		System.out.print("Size=" + intLinkedList.size());
+		
+		
+		
+
+	}
+
+}
